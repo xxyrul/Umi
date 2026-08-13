6706bc671cca724843fdff2b47931769d17020dc
Azrul Baharum <norazrul7@gmail.com>
2026-08-13 14:57:35 +0800

    Fix metro config blacklist regex preventing expo-router resolution

--- metro.config.js
+++ metro.config.js
@@ -3,6 +3,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 // Block native build files from being watched by Metro to prevent crashes
-config.resolver.blacklistRE = /.*\/android\/.*|.*\/ios\/.*|node_modules\/.*\/build\/.*/;
+config.resolver.blockList = [
+  /.*\/android\/build\/.*/,
+  /.*\/ios\/build\/.*/,
+];
 
 module.exports = config;
