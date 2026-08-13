c2f75da21a7f7e20192ee5009f41d8f60536af96
xxyrul <100736245+xxyrul@users.noreply.github.com>
2026-08-14 01:53:23 +0800

    Sync latest Replit app changes

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Block native build files from being watched by Metro to prevent crashes
-config.resolver.blockList = [
-  /.*\/android\/build\/.*/,
-  /.*\/ios\/build\/.*/,
-];
-
 module.exports = config;
